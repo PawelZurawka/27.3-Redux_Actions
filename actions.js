866a0c40f31874c89010cc96db05cb4cc887f01e
Paweł Żurawka <pzurawka@gmail.com>
2019-08-19 13:09:15 +0200

    Add uuid, export consts and functions. Delete dispatches

--- actions.js
+++ actions.js
@@ -1,47 +1,44 @@
-const ADD_COMMENT = 'ADD_COMMENT';
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-const EDIT_COMMENT = 'EDIT_COMMENT';
-const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
-const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+import uuid from 'uuid';
 
-function addComment(text) {
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+export function addComment(text) {
   return {
     type: ADD_COMMENT,
     text,
     id: uuid.v4()
   };
 }
-const boundAddComment = text => dispatch(addComment(text));
 
-function removeComment(id) {
+export function removeComment(id) {
   return {
     type: REMOVE_COMMENT,
     id
   };
 }
-const boundRemoveComment = id => dispatch(removeComment(id));
 
-function editComment(id, text) {
+export function editComment(id, text) {
   return {
     type: EDIT_COMMENT,
     id,
     text
   };
 }
-const boundEditComment = (id, text) => dispatch(editComment(id, text));
 
-function thumbUpComment(id) {
+export function thumbUpComment(id) {
   return {
     type: THUMB_UP_COMMENT,
     id
   };
 }
-const boundThumbUpComment = id => dispatch(ThumbUpComment(id));
 
-function thumbDownComment(id) {
+export function thumbDownComment(id) {
   return {
     type: THUMB_DOWN_COMMENT,
     id
   };
 }
-const boundThumbDownComment = id => dispatch(ThumbDownComment(id));
